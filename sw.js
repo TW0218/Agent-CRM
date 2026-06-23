891356e8f6497c0dc8728d8cf7fa9c4c40b0ab27
TW0218 <cr28rh@gmail.com>
2026-06-23 20:39:47 +0900

    DB.load()のクエリをSupabase側でPDF除外に修正・タイムアウト解消

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'agent-crm-v11';
+const CACHE = 'agent-crm-v12';
 const ASSETS = ['/', '/index.html', '/manifest.webmanifest', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
